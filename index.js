c72a1fc1b048dc4d6832543de4f6290ed80b02af
Asadbek Azamjonov <asadbek010azamjonov@gmail.com>
2022-06-08 22:06:35 +0200

    2 done

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 function Header(title) {
-  console.log(`\n----->>${title.toUpperCase()}----------`);
+  console.log(`\n----->> ${title.toUpperCase()}----------`);
 }
-// Write function with one param and that should be multiplied by
+// Write function with one param and that should be multiplied by 2
 Header("question Number 1");
 const multipliedBy2 = (num) => {
   return num * 2;
 };
-
 console.log(multipliedBy2(10));
+
+// Write a function called whatDayIsIt that should return the current day of the week.
+Header("question Number 2");
+
+const getDay = () => {
+  let today = new Date();
+  let dayList = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  let result = dayList[today.getDay()];
+  console.log(result);
+};
+getDay();

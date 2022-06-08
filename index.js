a30ce3054db3563e5bea4a8fd30a494888193407
Asadbek Azamjonov <asadbek010azamjonov@gmail.com>
2022-06-08 21:58:12 +0200

    done

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+function Header(title) {
+  console.log(`\n----->>${title.toUpperCase()}----------`);
+}
 // Write function with one param and that should be multiplied by
-
+Header("question Number 1");
 const multipliedBy2 = (num) => {
   return num * 2;
 };

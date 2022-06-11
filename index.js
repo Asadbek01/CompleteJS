a9402aae090f6f9239dfe731b4f8b08bfd6c19aa
Asadbek Azamjonov <asadbek010azamjonov@gmail.com>
2022-06-11 02:54:36 +0200

    done

--- index.js
+++ index.js
@@ -131,3 +131,20 @@ function f() {
 }
 f();
 console.log(a);
+let a = 10;
+function f() {
+  if (true) {
+    let b = 9;
+
+    // It prints 9
+    console.log(b);
+  }
+
+  // It gives error as it
+  // defined in if block
+  console.log(b);
+}
+f();
+
+// It prints 10
+console.log(a);

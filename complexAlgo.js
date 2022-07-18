4a391d3a21bf071d8d7f78379fc0223565e0e25f
Asadbek <asadbek010azamjonov@gmail.com>
2022-07-18 13:37:58 +0200

    done git commit -m done git commit -m done

--- complexAlgo.js
+++ complexAlgo.js
@@ -46,7 +46,8 @@ function makeTags(tag, word){
   }
   console.log(makeTags("span", "hi"))
   
-//   
+//   Given a string, return a new string made of 3 copies of the 
+// last 2 chars of the original string. The string length will be at least 2.
 function extraEnd(str){
     const res = str.substring(str.length -2)
     return res+ res +res
